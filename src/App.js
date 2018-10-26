86e60bca7f7d372c0c0901d6b76ff508112dc187
iamhimadri <himadrighosh13@gmail.com>
2018-10-27 03:23:45 +0530

    COOL DRAW (Task #2): Implement the logic to draw lines on this canvas.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,42 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef();
+    this.state = {
+      isDrawing: false
+    }
+  }
+
+  _getContext = () => {
+    const el = this.canvasRef.current;
+    return el.getContext("2d");
+  }
+
+  _handleOnMouseDown = (e) => {
+    const ctx = this._getContext();
+    const { offsetX, offsetY } = e.nativeEvent;
+    this.setState({ isDrawing: true }, () => {
+      // Moving staring point for a new line.
+      ctx.moveTo(offsetX, offsetY);
+    });
+  }
+
+  _handleOnMouseMove = (e) => {
+    const { isDrawing } = this.state;
+    const ctx = this._getContext();
+    if (isDrawing) {
+      // Connecting the last point with current coordinates
+      ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+      ctx.stroke();
+    }
+  }
+
+  _handleOnMouseUp = () => {
+    this.setState({ isDrawing: false });
+  }
+
   render() {
     return (
       <div className="App">
@@ -9,7 +45,15 @@ class App extends Component {
           <p> SUPER EFFICIENT DRAWING BOARD </p>
         </header>
         <div className="drawing-container">
-          <canvas id="drawing-board" width="700" height="400"></canvas>
+          <canvas
+            id="drawing-board"
+            width="700"
+            height="400"
+            ref={this.canvasRef}
+            onMouseDown={this._handleOnMouseDown}
+            onMouseMove={this._handleOnMouseMove}
+            onMouseUp={this._handleOnMouseUp}
+          />
         </div>
       </div>
     );
