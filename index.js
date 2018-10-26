7cb2bc1ee068863bf21c4d6765d166771ece707f
Karthik M A M <karthik_m_a_m@outlook.com>
2018-10-27 02:07:53 +0530

    add logic to update distance covered

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 (function () {
   const canvas = document.getElementById('canvas')
+  const distance = document.getElementById('distance')
   const canvasContext = canvas.getContext('2d')
 
   let startX = 0
   let startY = 0
   let recordingStarted = false
+  let totalDistance = 0
 
   const recordStart = (e) => {
     recordingStarted = true
-    
+
+    // Record the start of the line
     const boundingRect = e.target.getBoundingClientRect()
     startX = e.clientX - boundingRect.left
     startY = e.clientY - boundingRect.top
@@ -18,15 +21,21 @@
     if (recordingStarted) {
       recordingStarted = false
 
+      // Record the end of the line
       const boundingRect = e.target.getBoundingClientRect()
       const endX = e.clientX - boundingRect.left
       const endY = e.clientY - boundingRect.top
 
+      // Draw the line
       canvasContext.beginPath()
       canvasContext.strokeStyle = 'black'
       canvasContext.moveTo(startX, startY)
       canvasContext.lineTo(endX, endY)
       canvasContext.stroke()
+
+      // Update the distance covered
+      totalDistance += Math.sqrt((endX - startX) * (endX - startX) + (endY - startY) * (endY - startY))
+      distance.innerText = `Distance Covered: ${Math.round(totalDistance * 100) / 100} m`
     }
   }
 
