5c48ada9a52f54805520fab2385c25f0fa066e50
iamhimadri <himadrighosh13@gmail.com>
2018-10-27 09:12:13 +0530

    REPLY (Task #1, #2 & #3): Store Event Data, Replay (Still Need a bit optimization)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ class App extends Component {
     this.canvasRef = React.createRef();
     this.state = {
       isDrawing: false,
-      staringCoodinates: null,
       distance: 0
     }
   }
@@ -18,37 +17,108 @@ class App extends Component {
     return el.getContext("2d");
   }
 
-  _handleOnMouseDown = (e) => {
+  _handleResetCanvas = () => {
     const ctx = this._getContext();
-    const { offsetX, offsetY } = e.nativeEvent;
-    this.setState({ isDrawing: true, staringCoodinates: [offsetX, offsetY] }, () => {
-      // Moving staring point for a new line.
-      ctx.moveTo(offsetX, offsetY);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    this.setState({ distance: 0 });
+  }
+
+  _handleRecord = (coodinates) => {
+    const recordLen = this.records.length;
+    
+    if (recordLen > 0) {
+      const gap = Date.now() - this.staredAt;
+      const last = this.records[recordLen - 1];
+      if (last[0] === coodinates[0]
+        && last[1] === coodinates[1]
+        && last[2] === coodinates[2]) {
+        this.records[recordLen - 1][3].push(gap);
+        return;
+      } else {
+        coodinates.push([gap])
+      }
+    } else {
+      this.staredAt = Date.now();
+      coodinates.push([0]);
+    }
+    this.records.push(coodinates);
+  }
+
+  _handleUpdateDistance = () => {
+    const len = this.records.length;
+    const P = this.records[len - 2];
+    const Q = this.records[len - 1];
+    const distance = distanceBetweenCoordinates(P[0], P[1], Q[0], Q[1]);
+    this.setState({ distance: this.state.distance + distance }); 
+  }
+
+  _handleReply = () => {
+    this._handleResetCanvas();
+    this.records.map((record) => {
+      return record[3].map((diff) => {
+        return setTimeout(() => {
+          this._handleDraw(record)
+        }, diff)
+      })
+    });
+  }
+
+  _handleDraw = (coodinates) => {
+    const ctx = this._getContext();
+
+    const type = coodinates[2];
+    switch(type) {
+      case "D":
+        ctx.beginPath();
+        ctx.moveTo(coodinates[0], coodinates[1]);
+        break;
+      case "M":
+        // Connecting the last point with current coordinates
+        ctx.lineTo(coodinates[0], coodinates[1]);
+        ctx.stroke();
+        this._handleUpdateDistance();
+        break;
+    default:
+        // Nothing
+    }
+  }
+
+  _handleOnMouseDown = (e) => {
+    // [X, Y, Type: (Down)]
+    const coodinates = [
+      e.nativeEvent.offsetX,
+      e.nativeEvent.offsetY,
+      "D"
+    ];
+    this.setState({ isDrawing: true }, () => {
+      this._handleRecord(coodinates);
+      this._handleDraw(coodinates);
     });
   }
 
   _handleOnMouseMove = (e) => {
-    const { isDrawing, staringCoodinates } = this.state;
-    const ctx = this._getContext();
-    const { offsetX, offsetY } = e.nativeEvent;
-    if (isDrawing) {
-      // Connecting the last point with current coordinates
-      ctx.lineTo(offsetX, offsetY);
-      ctx.stroke();
+    const { isDrawing } = this.state;
+    // [X, Y, Type: (Move)]
+    const coodinates = [
+      e.nativeEvent.offsetX,
+      e.nativeEvent.offsetY,
+      "M"
+    ];
 
-      // Updating live distance
-      const distance = distanceBetweenCoordinates(
-        staringCoodinates[0],
-        staringCoodinates[1],
-        offsetX,
-        offsetY)
-      this.setState({ distance: parseFloat(distance).toFixed(2) });
+    if (isDrawing) {
+      this._handleRecord(coodinates);
+      this._handleDraw(coodinates);
     }
   }
 
   _handleOnMouseUp = () => {
+    const ctx = this._getContext();
+    ctx.closePath();
     this.setState({ isDrawing: false });
   }
+
+  records = [];
+  staredAt = null;
 
   render() {
     return (
@@ -57,6 +127,10 @@ class App extends Component {
           <p> SUPER EFFICIENT DRAWING BOARD </p>
         </header>
         <div className="drawing-container">
+          <div className="action-container">
+            <button onClick={this._handleResetCanvas}> Reset </button>
+            <button onClick={this._handleReply}> Play </button>
+          </div>
           <canvas
             id="drawing-board"
             width="700"
