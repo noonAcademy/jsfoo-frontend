debd51acaf7a7e347a458e40ac3c6a1fec604b5e
Jagadish K <k.jagdish@gmail.com>
2018-10-27 08:22:16 +0530

    removed package.json and lint

--- replay-canvas.js
+++ replay-canvas.js
@@ -1,210 +1,211 @@
-const DONT_STORE = false
+const DONT_STORE = false;
 
 const getAbsolutePosition = (element) => {
-  var r = { x: element.offsetLeft, y: element.offsetTop }
+  const r = { x: element.offsetLeft, y: element.offsetTop };
   if (element.offsetParent) {
-    var tmp = getAbsolutePosition(element.offsetParent)
-    r.x += tmp.x
-    r.y += tmp.y
+    const tmp = getAbsolutePosition(element.offsetParent);
+    r.x += tmp.x;
+    r.y += tmp.y;
   }
-  return r
-}
+  return r;
+};
 
 const getRelativeCoordinates = (event, reference) => {
-  const el = event.target || event.srcElement
-  let pos
-  let x, y
-  let e
+  const el = event.target || event.srcElement;
+  let pos;
+  let x; let
+    y;
+  let e;
 
   if (!window.opera && typeof event.offsetX !== 'undefined') {
     // Use offset coordinates and find common offsetParent
-    pos = { x: event.offsetX, y: event.offsetY }
+    pos = { x: event.offsetX, y: event.offsetY };
 
     // Send the coordinates upwards through the offsetParent chain.
-    e = el
+    e = el;
     while (e) {
-      e.mouseX = pos.x
-      e.mouseY = pos.y
-      pos.x += e.offsetLeft
-      pos.y += e.offsetTop
-      e = e.offsetParent
+      e.mouseX = pos.x;
+      e.mouseY = pos.y;
+      pos.x += e.offsetLeft;
+      pos.y += e.offsetTop;
+      e = e.offsetParent;
     }
 
     // Look for the coordinates starting from the reference element.
-    e = reference
-    const offset = { x: 0, y: 0 }
+    e = reference;
+    const offset = { x: 0, y: 0 };
     while (e) {
       if (typeof e.mouseX !== 'undefined') {
-        x = e.mouseX - offset.x
-        y = e.mouseY - offset.y
-        break
+        x = e.mouseX - offset.x;
+        y = e.mouseY - offset.y;
+        break;
       }
-      offset.x += e.offsetLeft
-      offset.y += e.offsetTop
-      e = e.offsetParent
+      offset.x += e.offsetLeft;
+      offset.y += e.offsetTop;
+      e = e.offsetParent;
     }
 
     // Reset stored coordinates
-    e = el
+    e = el;
     while (e) {
-      e.mouseX = undefined
-      e.mouseY = undefined
-      e = e.offsetParent
+      e.mouseX = undefined;
+      e.mouseY = undefined;
+      e = e.offsetParent;
     }
   } else {
     // Use absolute coordinates
-    pos = getAbsolutePosition(reference)
-    x = event.pageX - pos.x
-    y = event.pageY - pos.y
+    pos = getAbsolutePosition(reference);
+    x = event.pageX - pos.x;
+    y = event.pageY - pos.y;
   }
   // Subtract distance to middle
-  return { x: x, y: y }
-}
+  return { x, y };
+};
 
-const canvas = document.getElementById('cool-draw')
-canvas.setAttribute('width', document.getElementsByClassName('container')[0].clientWidth)
+const canvas = document.getElementById('cool-draw');
+canvas.setAttribute('width', document.getElementsByClassName('container')[0].clientWidth);
 
-const ctx = canvas.getContext('2d')
-const payloadDisplay = document.getElementById('payload')
-const distanceDisplay = document.getElementById('distance-calculator')
-let showPayload = false
-let clickedPoints = []
-let mousepressed = false
-let distanceTravelled = 0
-let start
+const ctx = canvas.getContext('2d');
+const payloadDisplay = document.getElementById('payload');
+const distanceDisplay = document.getElementById('distance-calculator');
+const showPayload = false;
+let clickedPoints = [];
+let mousepressed = false;
+let distanceTravelled = 0;
+let start;
 
-const formsTriangle = (ptA, ptB, ptC) => ptA[0] * (ptB[1] - ptC[1]) + ptB[0] * (ptC[1] - ptA[1]) + ptC[0] * (ptA[1] - ptB[1])
+const formsTriangle = (ptA, ptB, ptC) => ptA[0] * (ptB[1] - ptC[1]) + ptB[0] * (ptC[1] - ptA[1]) + ptC[0] * (ptA[1] - ptB[1]);
 
 const updateDistance = (num = clickedPoints.length) => {
   if (num > 1) {
-    let p1 = clickedPoints[num - 2]
-    let p2 = clickedPoints[num - 1]
-    distanceTravelled += Math.hypot(p2[0] - p1[0], p2[1] - p2[1])
+    const p1 = clickedPoints[num - 2];
+    const p2 = clickedPoints[num - 1];
+    distanceTravelled += Math.hypot(p2[0] - p1[0], p2[1] - p2[1]);
   }
-  distanceDisplay.innerHTML = `Drawn ${distanceTravelled} meters.`
-}
+  distanceDisplay.innerHTML = `Drawn ${distanceTravelled} meters.`;
+};
 
 const addPoint = (obj, index) => {
   if (!clickedPoints.length) {
-    start = Date.now()
-    obj.push(0)
+    start = Date.now();
+    obj.push(0);
   } else {
-    obj.push(Date.now() - start)
+    obj.push(Date.now() - start);
   }
 
   if (index) {
-    clickedPoints[index] = obj
+    clickedPoints[index] = obj;
   } else {
-    clickedPoints.push(obj)
+    clickedPoints.push(obj);
   }
-  displayDistance()
-}
+  displayDistance();
+};
 
 const displayDistance = () => {
   if (showPayload) {
-    payloadDisplay.innerHTML = JSON.stringify(clickedPoints, null, 2)
+    payloadDisplay.innerHTML = JSON.stringify(clickedPoints, null, 2);
   } else {
-    payloadDisplay.innerHTML = ''
+    payloadDisplay.innerHTML = '';
   }
-}
+};
 
 const draw = (pt, index) => {
   if (pt[2] === 'c') {
-    ctx.beginPath()
-    ctx.moveTo(pt[0], pt[1])
-    mousepressed = true
+    ctx.beginPath();
+    ctx.moveTo(pt[0], pt[1]);
+    mousepressed = true;
   } else if (pt[2] === 'd' && mousepressed) {
-    ctx.lineTo(pt[0], pt[1])
-    ctx.stroke()
-    updateDistance(index)
+    ctx.lineTo(pt[0], pt[1]);
+    ctx.stroke();
+    updateDistance(index);
   } else if (pt[2] === 'r') {
-    mousepressed = false
-    ctx.closePath()
+    mousepressed = false;
+    ctx.closePath();
   } else if (pt[2] === 'w') {
-    mousepressed = false
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    mousepressed = false;
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
   }
-}
+};
 
-const mouseRecorder = ev => {
-  const target = ev.target || ev.srcElement
-  const coords = getRelativeCoordinates(ev, target)
-  let obj = [
+const mouseRecorder = (ev) => {
+  const target = ev.target || ev.srcElement;
+  const coords = getRelativeCoordinates(ev, target);
+  const obj = [
     coords.x,
-    coords.y
-  ]
+    coords.y,
+  ];
 
   switch (ev.type) {
     case 'mousedown':
     case 'mouseup':
 
-      obj.push(ev.type === 'mousedown' ? 'c' : 'r') // clicked = 'c' ; releases = 'r'
-      addPoint(obj)
-      break
+      obj.push(ev.type === 'mousedown' ? 'c' : 'r'); // clicked = 'c' ; releases = 'r'
+      addPoint(obj);
+      break;
 
     case 'mousemove':
-      obj.push('d') // dragged = 'd'
+      obj.push('d'); // dragged = 'd'
       if (mousepressed) {
         if (clickedPoints.length > 2) {
-          const numPointsRegistered = clickedPoints.length
+          const numPointsRegistered = clickedPoints.length;
 
           if (
-            clickedPoints[numPointsRegistered - 1][2] !== 'c' &&
-            !formsTriangle(
+            clickedPoints[numPointsRegistered - 1][2] !== 'c'
+            && !formsTriangle(
               clickedPoints[numPointsRegistered - 2],
               clickedPoints[numPointsRegistered - 1],
-              obj
+              obj,
             )
           ) {
-            addPoint(obj, numPointsRegistered - 1)
+            addPoint(obj, numPointsRegistered - 1);
           } else {
-            addPoint(obj)
+            addPoint(obj);
           }
         } else {
-          addPoint(obj)
+          addPoint(obj);
         }
       }
   }
 
-  draw(obj)
-}
+  draw(obj);
+};
 
 const clearCanvas = (store = true) => {
-  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-  distanceTravelled = 0
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  distanceTravelled = 0;
   if (store) {
-    addPoint([null, null, 'w'])
+    addPoint([null, null, 'w']);
   }
-  updateDistance()
-}
+  updateDistance();
+};
 
 const shred = () => {
-  clearCanvas()
+  clearCanvas();
 
-  clickedPoints = []
-}
+  clickedPoints = [];
+};
 
 const replay = () => {
-  const num = clickedPoints.length
-  clearCanvas(DONT_STORE)
-  canvas.removeEventListener('mousedown', mouseRecorder)
-  canvas.removeEventListener('mousemove', mouseRecorder)
-  canvas.removeEventListener('mouseup', mouseRecorder)
+  const num = clickedPoints.length;
+  clearCanvas(DONT_STORE);
+  canvas.removeEventListener('mousedown', mouseRecorder);
+  canvas.removeEventListener('mousemove', mouseRecorder);
+  canvas.removeEventListener('mouseup', mouseRecorder);
   clickedPoints.map((pt, index) => {
     setTimeout(() => {
-      draw(pt, index)
+      draw(pt, index);
 
       if (index === num - 1) {
-        canvas.addEventListener('mousedown', mouseRecorder)
-        canvas.addEventListener('mousemove', mouseRecorder)
-        canvas.addEventListener('mouseup', mouseRecorder)
+        canvas.addEventListener('mousedown', mouseRecorder);
+        canvas.addEventListener('mousemove', mouseRecorder);
+        canvas.addEventListener('mouseup', mouseRecorder);
       }
-    }, pt[3])
-  })
-}
+    }, pt[3]);
+  });
+};
 
-clearCanvas(DONT_STORE)
+clearCanvas(DONT_STORE);
 
-canvas.addEventListener('mousedown', mouseRecorder)
-canvas.addEventListener('mousemove', mouseRecorder)
-canvas.addEventListener('mouseup', mouseRecorder)
+canvas.addEventListener('mousedown', mouseRecorder);
+canvas.addEventListener('mousemove', mouseRecorder);
+canvas.addEventListener('mouseup', mouseRecorder);
