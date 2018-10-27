580ca74c3f2292476ba1c58d8cd7b78cd60d26dd
Karthik M A M <karthik_m_a_m@outlook.com>
2018-10-27 10:46:33 +0530

    add basic animation logic

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
   let startX = 0
   let startY = 0
   let recordingStarted = false
+  let endX = 0
+  let endY = 0
 
   let totalDistance = 0
 
@@ -35,6 +37,40 @@
     startY = event.clientY - boundingRect.top
   }
 
+  const recordDrag = (event) => {
+    if (recordingStarted) {
+      // Compute time delay
+      const endTime = new Date()
+      const timeDifference = endTime - startTime
+      startTime = endTime
+
+      // Compute the end of the line
+      const boundingRect = event.target.getBoundingClientRect()
+      endX = event.clientX - boundingRect.left
+      endY = event.clientY - boundingRect.top
+
+      // Clear canvas
+      canvasContext.clearRect(0, 0, canvas.width, canvas.height)
+
+      // Repaint lines
+      for (const [_, startX, startY, endX, endY, isEndpoint] of events) {
+        if (isEndpoint) {
+          drawLine(startX, startY, endX, endY);
+        }
+      }
+
+      // Draw the line
+      drawLine(startX, startY, endX, endY)
+
+      // Record the event
+      events.push([timeDifference, startX, startY, endX, endY, false])
+
+      // Uncomment for free hand drawing
+      // startX = endX
+      // startY = endY
+    }
+  }
+
   const recordStop = (event) => {
     if (recordingStarted) {
       recordingStarted = false
@@ -46,8 +82,8 @@
 
       // Compute the end of the line
       const boundingRect = event.target.getBoundingClientRect()
-      const endX = event.clientX - boundingRect.left
-      const endY = event.clientY - boundingRect.top
+      endX = event.clientX - boundingRect.left
+      endY = event.clientY - boundingRect.top
 
       // Draw the line
       drawLine(startX, startY, endX, endY)
@@ -56,7 +92,7 @@
       updateTotalDistance(startX, startY, endX, endY)
 
       // Record the event
-      events.push([timeDifference, startX, startY, endX, endY])
+      events.push([timeDifference, startX, startY, endX, endY, true])
     }
   }
 
@@ -68,7 +104,7 @@
     }
 
     // Get current event data
-    const [timeDifference, startX, startY, endX, endY] = events[eventId]
+    const [timeDifference, startX, startY, endX, endY, isEndpoint] = events[eventId]
 
     // Reset canvas and distance
     if (eventId === 0) {
@@ -78,11 +114,32 @@
     }
 
     setTimeout(() => {
-      // Draw the line
-      drawLine(startX, startY, endX, endY)
+      // Animate drawing of the lines
+      for (let currentEventId in events) {
+        if (currentEventId > eventId) break
+
+        const [_, startX, startY, endX, endY, isEndpoint] = events[currentEventId]
+
+        canvasContext.clearRect(0, 0, canvas.width, canvas.height)
+
+        drawLine(startX, startY, endX, endY)
+      }
+
+      // Redraw the lines
+      for (let currentEventId in events) {
+        if (currentEventId > eventId) break
+
+        const [_, startX, startY, endX, endY, isEndpoint] = events[currentEventId]
+
+        if (!isEndpoint) continue
+
+        drawLine(startX, startY, endX, endY)
+      }
 
       // Update the distance covered
-      updateTotalDistance(startX, startY, endX, endY)
+      if (isEndpoint) {
+        updateTotalDistance(startX, startY, endX, endY)
+      }
 
       // Replay next event
       replayEvent(eventId + 1)
@@ -91,6 +148,7 @@
 
   // Add listeners
   canvas.addEventListener('mousedown', recordStart)
+  canvas.addEventListener('mousemove', recordDrag)
   canvas.addEventListener('mouseleave', recordStop)
   canvas.addEventListener('mouseup', recordStop)
   replay.addEventListener('click', () => replayEvent())
